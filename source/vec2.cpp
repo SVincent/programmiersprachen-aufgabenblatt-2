84b147ed18a28587d1628179f62085e5c906d927
Vincent Soellner <soellner.vincent@gmail.com>
2018-05-07 00:59:34 +0200

    task 2.4

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -31,7 +31,7 @@
 		if (s == 0.0) {
             //exception, as division by zero isn't defined
             this->x_ = 99999.99;
-            this->y_ = 99999.99
+            this->y_ = 99999.99;
 			return *this;
 		}
 		else {
@@ -39,4 +39,36 @@
 			this->y_ /= s;
 			return *this;
 		}
+	};
+
+	Vec2 operator +(Vec2 const& a, Vec2 const& b) {
+		Vec2 temp(a.x_ + b.x_, a.y_ + b.y_);
+		return temp;
+	};
+
+	Vec2 operator -(Vec2 const& a, Vec2 const& b) {
+		Vec2 temp(a.x_ - b.x_, a.y_ - b.y_);
+		return temp;
+	};
+
+	Vec2 operator *(Vec2 const& v, float s) {
+		Vec2 temp(v.x_ * s, v.y_*s);
+		return temp;
+	};
+
+	Vec2 operator /(Vec2 const& v, float s) {
+		if (s == 0) {
+            //exception, as division by zero isn't defined
+            Vec2 exc(99999.99,99999.99);
+			return exc;
+		}
+		else {
+			Vec2 temp(v.x_ / s, v.y_ / s);
+			return temp;
+		}
+	};
+
+	Vec2 operator *(float s, Vec2 const& v){
+		Vec2 temp(s * v.x_ , s * v.y_);
+		return temp;
 	};
--- source/vec2.hpp
+++ source/vec2.hpp
@@ -1,5 +1,5 @@
-# ifndef VEC2_HPP
-# define VEC2_HPP
+#ifndef VEC2_HPP
+#define VEC2_HPP
 // Vec2 class definition
 struct Vec2
 {
@@ -13,6 +13,12 @@ struct Vec2
 	Vec2& operator -=(Vec2 const& v);
 	Vec2& operator *=(float s);
 	Vec2& operator /=(float s);
-    
 };
-# endif // VEC2_HPP
+
+Vec2 operator +(Vec2 const & u, Vec2 const & v);
+Vec2 operator -(Vec2 const & u, Vec2 const & v);
+Vec2 operator *(Vec2 const & v, float s);
+Vec2 operator /(Vec2 const & v, float s);
+Vec2 operator *(float s, Vec2 const & v);
+
+#endif // VEC2_HPP
