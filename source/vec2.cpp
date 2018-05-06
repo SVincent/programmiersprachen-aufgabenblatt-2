a4f2fd2849e20ede2fbc458aa19fb1cfbd4aac93
Vincent Soellner <soellner.vincent@gmail.com>
2018-05-06 23:04:50 +0200

    task 2.1 made changes to CMakeLists

---
+++ source/vec2.cpp
@@ -0,0 +1,11 @@
+#include "vec2.hpp"
+
+void vec2::vec2(){
+    x = 0;
+    y = 0;
+}
+
+void vec2::vec2(int inpx, int inpy){
+    x = inpx;
+    y = inpy;
+}
---
+++ source/vec2.hpp
@@ -0,0 +1,10 @@
+# ifndef VEC2_HPP
+# define VEC2_HPP
+// Vec2 class definition
+struct Vec2
+{
+// TODO Constructors
+float x;
+float y;
+};
+# endif // VEC2_HPP
