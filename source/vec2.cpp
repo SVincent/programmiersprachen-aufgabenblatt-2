b3b4661d35256bf301f6eedc8681162bb97e3074
Vincent Soellner <soellner.vincent@gmail.com>
2018-05-07 00:52:12 +0200

    Add vec2

--- source/vec2.cpp
+++ source/vec2.cpp
@@ -7,3 +7,36 @@
 	Vec2::Vec2(float x, float y) : //constructor with assigned values
 		x_{ x },
 		y_{ y } {}
+
+	Vec2& Vec2::operator+=(Vec2 const& v) {
+		this->x_ += v.x_; //the membervariable x_ gets increased by v.x, with this being the vec2 which the operator gets used on
+		this->y_ += v.y_;
+		return *this;
+	};
+
+	Vec2& Vec2::operator-=(Vec2 const& v) {
+		this->x_ -= v.x_;
+		this->y_ -= v.y_;
+		return *this;
+	};
+
+	Vec2& Vec2::operator *=(float s) {
+		this->x_ *= s;
+		this->y_ *= s;
+		return *this;
+	};
+
+	Vec2& Vec2::operator /=(float s) {
+
+		if (s == 0.0) {
+            //exception, as division by zero isn't defined
+            this->x_ = 99999.99;
+            this->y_ = 99999.99
+			return *this;
+		}
+		else {
+			this->x_ /= s;
+			this->y_ /= s;
+			return *this;
+		}
+	};
