4197fa6d4852e4bfac2bbc111c9c94968441a658
Vincent Soellner <soellner.vincent@gmail.com>
2018-05-07 01:49:58 +0200

    task 2.5 added tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -161,3 +161,52 @@ TEST_CASE("/ operator", "[operator/]") {
 	REQUIRE(a3.x_ == Approx(99999.99).epsilon(0.001));
 	REQUIRE(a3.y_ == Approx(99999.99).epsilon(0.001));
 }
+
+// Task 2.5 tests
+Mat2 defmat
+
+// test for Mat2 default constructor
+TEST_CASE("default constructor Mat2", "[defmat]") {
+	REQUIRE(defmat.n[0][0] == 1);
+	REQUIRE(defmat.n[0][1] == 0);
+	REQUIRE(defmat.n[1][0] == 0);
+	REQUIRE(defmat.n[1][1] == 1);
+}
+
+// test for Mat2 user constructor
+TEST_CASE("user constructor Mat2", "[testmat]") {
+	Mat2 testmat(1, 2, 3, 4);
+
+	REQUIRE(matTwo.n[0][0] == 1);
+	REQUIRE(matTwo.n[0][1] == 2);
+	REQUIRE(matTwo.n[1][0] == 3);
+	REQUIRE(matTwo.n[1][1] == 4);
+}
+
+
+// test for *= operator
+TEST_CASE("operator*= Mat2", "[operator*=]") {
+	Mat2 testmat(1, 2, 3, 4);
+	Mat2 testmat2(5, 6, 7, 8);
+
+	testmat.operator*=(testmat2);
+
+	REQUIRE(testmat.n[0][0] == 19);
+	REQUIRE(testmat.n[0][1] == 22);
+	REQUIRE(testmat.n[1][0] == 43);
+	REQUIRE(testmat.n[1][1] == 50);
+}
+
+// test for * operator
+TEST_CASE("operator* Mat2", "[operator*]") {
+	Mat2 testmat(1, 2, 3, 4);
+	Mat2 testmat2(5, 6, 7, 8);
+	Mat2 matRes;
+
+	matRes = operator*(testmat, testmat2);
+
+	REQUIRE(matRes.n[0][0] == 19);
+	REQUIRE(matRes.n[0][1] == 22);
+	REQUIRE(matRes.n[1][0] == 43);
+	REQUIRE(matRes.n[1][1] == 50);
+}
