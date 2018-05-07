1c2fdece4d5e23eb08bb0a21f55a813162f9890e
Vincent Soellner <soellner.vincent@gmail.com>
2018-05-07 12:15:03 +0200

    Task 2.6 complete (including tests)

--- source/mat2.cpp
+++ source/mat2.cpp
@@ -41,3 +41,66 @@ Mat2 operator*(Mat2 const& m1, Mat2 const& m2) {
 	temp.n[1][1] = m1.n[1][0] * m2.n[0][1] + m1.n[1][1] * m2.n[1][1];
 	return temp;
 };
+
+// Task 2.6 
+
+// calculate the determinant
+float Mat2::det() const{
+	float detTemp = this->n[0][0] * this->n[1][1] - this->n[1][0] * this->n[0][1];
+	return detTemp;
+};
+
+// Matrix-vector multiplication operator
+Vec2 operator*(Mat2 const& m, Vec2 const& v) {
+	Vec2 tempVec;
+	tempVec.x_ = m.n[0][0] * v.x_ + m.n[0][1] * v.y_;
+	tempVec.y_ = m.n[1][0] * v.x_ + m.n[1][1] * v.y_;
+	return tempVec;
+};
+Vec2 operator*(Vec2 const& v, Mat2 const& m) {
+	Vec2 tempVec;
+	tempVec.x_ = m.n[0][0] * v.x_ + m.n[0][1] * v.y_;
+	tempVec.y_ = m.n[1][0] * v.x_ + m.n[1][1] * v.y_;
+	return tempVec;
+}
+
+// Matrix inverse calculation
+Mat2 inverse(Mat2 const& m) {
+
+	// exception: matrices with det == 0 can't be inverted
+	if (m.det() == 0) {
+		return m;
+	}
+
+	else {
+		Mat2 temp;
+		temp.n[0][0] = m.n[1][1] / m.det();
+		temp.n[0][1] = -1 * m.n[0][1] / m.det();
+		temp.n[1][0] = -1 * m.n[1][0] / m.det();
+		temp.n[1][1] = m.n[0][0] / m.det();
+
+		return temp;
+	}
+};
+
+// Matrix transpose 
+Mat2 transpose(Mat2 const & m) {
+	Mat2 temp;
+
+	temp.n[0][0] = m.n[0][0];
+	temp.n[0][1] = m.n[1][0];
+	temp.n[1][0] = m.n[0][1];
+	temp.n[1][1] = m.n[1][1];
+	return temp;
+};
+
+// Matrix rotation
+Mat2 make_rotation_mat2(float phi) {
+	Mat2 temp;
+
+	temp.n[0][0] = cos(phi);
+	temp.n[0][1] = -1 * sin (phi);
+	temp.n[1][0] = sin (phi);
+	temp.n[1][1] = cos (phi);
+	return temp;
+};
--- source/tests.cpp
+++ source/tests.cpp
@@ -210,3 +210,93 @@ TEST_CASE("operator* Mat2", "[operator*]") {
 	REQUIRE(matRes.n[1][0] == 43);
 	REQUIRE(matRes.n[1][1] == 50);
 }
+
+// Task 2.6
+
+// test for determinant calculation
+TEST_CASE("Mat2::det() test" "[Mat2::det]") {
+	Mat2 testmat(1, 2, 3, 4);
+	Mat2 testmat2(5, 6, 7, 9);
+
+	REQUIRE (testmat.det()== -2);
+	REQUIRE(testmat2.det() == 3);
+}
+
+// test for matrix * vector multiplikation
+TEST_CASE("mat2*vec2 test" "[operator*]") {
+	Mat2 testmat(1, 2, 3, 4);
+	Mat2 testmat2(5, 6, 7, 8);
+	Vec2 vec(2,1.4);
+	Vec2 vecEmpty;
+
+	Vec2 vecRes;
+	vecRes = operator*(testmat, vec);
+
+	REQUIRE(vecRes.x_== Approx(4.8).epsilon(0.01));
+	REQUIRE(vecRes.y_ == Approx(11.6).epsilon(0.01));
+
+	vecRes = operator*(vec, testmat2);
+	REQUIRE(vecRes.x_ == Approx(49.9).epsilon(0.01));
+	REQUIRE(vecRes.y_ == Approx(57.6).epsilon(0.01));
+
+	vecRes = operator*(vecEmpty, testmat2);
+	REQUIRE(vecRes.x_ == 0);
+	REQUIRE(vecRes.y_ == 0);
+}
+
+// test for matrix inversion
+TEST_CASE("inverse matrix test" "[inverse(Mat2 m)]") {
+	Mat2 testmat(1, 2, 3, 4);
+	Mat2 testmat2(5, 6, 7, 8);
+	Mat2 matRes;
+
+	matRes = inverse(testmat);
+	REQUIRE(matRes.n[0][0] == -2);
+	REQUIRE(matRes.n[0][1] == 1);
+	REQUIRE(matRes.n[1][0] == 1.5);
+	REQUIRE(matRes.n[1][1] == -0.5);
+
+	matRes = inverse(testmat2);
+	REQUIRE(matRes.n[0][0] == -4);
+	REQUIRE(matRes.n[0][1] == 3);
+	REQUIRE(matRes.n[1][0] == 3.5);
+	REQUIRE(matRes.n[1][1] == -2.5);
+}
+
+// test for transposed matrices
+TEST_CASE("transposed matrix test" "[transpose(Mat2 m)]") {
+	Mat2 testmat(1, 2, 3, 4);
+	Mat2 testmat2(5, 6, 7, 8);
+	Mat2 matRes;
+
+	matRes = transpose(testmat);
+	REQUIRE(matRes.n[0][0] == 1);
+	REQUIRE(matRes.n[0][1] == 3);
+	REQUIRE(matRes.n[1][0] == 2);
+	REQUIRE(matRes.n[1][1] == 4);
+
+	matRes = transpose(matThree);
+	REQUIRE(matRes.n[0][0] == 5);
+	REQUIRE(matRes.n[0][1] == 7);
+	REQUIRE(matRes.n[1][0] == 6);
+	REQUIRE(matRes.n[1][1] == 8);
+}
+
+// test for matrix rotation
+TEST_CASE("rotation matrix test" "[make_rotation_mat2(float phi)]") {
+	Mat2 matRes;
+
+	//~90°
+	matRes = make_rotation_mat2(1.570796);
+	REQUIRE(matRes.n[0][0] == Approx(0).epsilon(0.01));
+	REQUIRE(matRes.n[0][1] == Approx(-1).epsilon(0.01));
+	REQUIRE(matRes.n[1][0] == Approx(1).epsilon(0.01));
+	REQUIRE(matRes.n[1][1] == Approx(0).epsilon(0.01));
+
+	// ~123°
+	matRes = make_rotation_mat2(2.146755);
+	REQUIRE(matRes.n[0][0] == Approx(-0.5446).epsilon(0.0001));
+	REQUIRE(matRes.n[0][1] == Approx(-0.8386).epsilon(0.0001));
+	REQUIRE(matRes.n[1][0] == Approx(0.8386).epsilon(0.0001));
+	REQUIRE(matRes.n[1][1] == Approx(-0.5446).epsilon(0.0001));
+}
