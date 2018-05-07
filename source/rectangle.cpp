5ea540bb20c826d9ee957e38f8fc1ff1f7fcd40e
Vincent Soellner <soellner.vincent@gmail.com>
2018-05-07 13:24:18 +0200

    Task 2.8, created files, started work on rectangle

---
+++ source/rectangle.cpp
@@ -0,0 +1,30 @@
+#define _USE_MATH_DEFINES
+#include "rectangle.hpp"
+#include <math.h>
+
+Rectangle::Rectangle() {
+	Vec2 vec; //empty Vec2
+	max_ = vec;
+	min_ = vec;
+};
+
+Rectangle::Rectangle(Vec2 const& vec1, Vec2 const& vec2) {
+	max_ = vec1;
+	min_ = vec2;
+};
+
+Vec2 Rectangle::getMax() const{
+	return max_;
+};
+
+Vec2 Rectangle::getMin() const{
+	return min_;
+};
+
+void Rectangle::setMax(Vec2 const& vec) {
+    max_ = vec;
+}
+
+void Rectangle::setMin(Vec2 const& vec) {
+    min_ = vec;
+}
---
+++ source/rectangle.hpp
@@ -0,0 +1,24 @@
+#ifndef RECTANGLE_HPP
+#define RECTANGLE_HPP
+#include "vec2.hpp"
+
+class Rectangle {
+
+public:
+	Rectangle();
+	Rectangle(Vec2 const& vec1, Vec2 const& vec2);
+
+	// getter
+	Vec2 getMax() const;
+	Vec2 getMin() const;
+
+	// setter
+    void setMax(Vec2 const& vecmax);
+    void setMin(Vec2 const& vecmin);
+
+private:
+	Vec2 max_;
+	Vec2 min_;
+};
+
+#endif
